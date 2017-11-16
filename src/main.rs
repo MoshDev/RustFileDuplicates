f49a2ab0c6244fe95518be45b3a2193c5e174551
Mohammed Ersan <mosh_java@yahoo.com>
2017-11-16 02:03:05 +0000

    - initial commit - reading list of files under specific dir - printing files names & each one size in bytes

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::fs;
+use std::vec::Vec;
+use std::path::PathBuf;
+use std::path::Path;
+use std::os::macos::fs::MetadataExt;
+
+fn main() {
+    let dir_path = &find_arg_input_path();
+    let dir = Path::new(dir_path);
+    let files = list_dir_files(dir.to_path_buf());
+    for _file in files {
+        println!("{:?} | {:?}", _file.file_name().unwrap(), _file.metadata().unwrap().st_size())
+    }
+}
+
+pub fn find_arg_input_path() -> String {
+    let arg_input_path = env::args().nth(1).expect("Missing Argument");
+    return arg_input_path;
+}
+
+fn list_dir_files(input_path: PathBuf) -> Vec<PathBuf> {
+    let mut result_files: Vec<PathBuf> = Vec::new();
+
+    if input_path.is_file() {
+        result_files.push(input_path)
+    } else if input_path.is_dir() {
+        if let Ok(dir_files) = fs::read_dir(input_path) {
+            for file in dir_files.filter_map(|file| file.ok()) {
+                let file_path = file.path();
+                if file_path.is_file() {
+                    result_files.push(file_path)
+                } else if file_path.is_dir() {
+                    result_files.append(&mut list_dir_files(file_path))
+                }
+            }
+        }
+    }
+    return result_files;
+}
