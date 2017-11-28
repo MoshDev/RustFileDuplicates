0c85da938d6bb00af30ecbef17d4c9d1a3ffeda2
Mohammed Ersan <mosh_java@yahoo.com>
2017-11-28 20:34:54 +0000

    - move functions to files module

--- src/files.rs
+++ src/files.rs
@@ -1,5 +1,5 @@
 use std::path::PathBuf;
-use std::ffi::OsStr;
+use std::fs;
 
 pub struct FileInfo {
     pub file_name: String,
@@ -13,4 +13,24 @@ impl FileInfo {
             file_size: p.symlink_metadata().unwrap().len()
         };
     }
+}
+
+pub fn list_dir_files(input_path: PathBuf) -> Vec<PathBuf> {
+    let mut result_files: Vec<PathBuf> = Vec::new();
+
+    if input_path.is_file() {
+        result_files.push(input_path)
+    } else if input_path.is_dir() {
+        if let Ok(dir_files) = fs::read_dir(input_path) {
+            for file in dir_files.filter_map(|file| file.ok()) {
+                let file_path = file.path();
+                if file_path.is_file() {
+                    result_files.push(file_path)
+                } else if file_path.is_dir() {
+                    result_files.append(&mut list_dir_files(file_path))
+                }
+            }
+        }
+    }
+    return result_files;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-#[allow(non_snake_case)]
 use std::env;
-use std::fs;
 use std::vec::Vec;
 use std::path::PathBuf;
-use std::path::Path;
-use std::os::macos::fs::MetadataExt;
 
 mod files;
 
@@ -19,36 +15,15 @@ pub fn find_arg_input_path() -> Option<String> {
     return env::args().collect::<Vec<_>>().get(1).cloned();
 }
 
-fn list_dir_files(input_path: PathBuf) -> Vec<PathBuf> {
-    let mut result_files: Vec<PathBuf> = Vec::new();
-
-    if input_path.is_file() {
-        result_files.push(input_path)
-    } else if input_path.is_dir() {
-        if let Ok(dir_files) = fs::read_dir(input_path) {
-            for file in dir_files.filter_map(|file| file.ok()) {
-                let file_path = file.path();
-                if file_path.is_file() {
-                    result_files.push(file_path)
-                } else if file_path.is_dir() {
-                    result_files.append(&mut list_dir_files(file_path))
-                }
-            }
-        }
-    }
-    return result_files;
-}
 
 fn process_dir(input_path: String) -> () {
     println!("{:?}", input_path);
 
-    let dir = Path::new(&input_path);
-    let dir_files = list_dir_files(dir.to_path_buf());
+    let dir = PathBuf::from(&input_path);
+    let dir_files = files::list_dir_files(dir);
     let mapped: Vec<files::FileInfo> = dir_files.iter().map(|p| files::FileInfo::new(p)).collect();
+    println!("Size of list: {}",mapped.len());
     for _file in mapped {
-        println!("{filename:?} : {filesize:?}", filename = _file.file_name, filesize = _file.file_size);
+        //        println!("{filename:?} : {filesize:?}", filename = _file.file_name, filesize = _file.file_size);
     }
-    //    for _file in dir_files {
-    //                println!("{filename:?} | {filesize:?}", filename = _file.file_name().unwrap(), filesize = _file.metadata().unwrap().st_size());
-    //    }
 }
